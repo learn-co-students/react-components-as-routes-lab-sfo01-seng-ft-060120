38c8ecd02492f117aa46f86ecd38f01d628756cd
sfespinosa <scottjames.espinosa@gmail.com>
2020-08-12 11:55:49 -0700

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const generateActors = () => {
+    return actors.map(actor => {
+      return( 
+        <div>
+          <h2>{actor.name}</h2>
+          <p>
+            <ul>
+              {actor.movies.map(movie => <li>{movie}</li>)}
+            </ul>
+          </p>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {generateActors()}
     </div>
   );
 };
